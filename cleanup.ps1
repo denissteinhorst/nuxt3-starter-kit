4de2359aa89485f4fb563aaf2137292e16a71983
Denis Steinhorst <denis@steinhor.st>
2023-04-15 17:50:19 +0200

    Update: "Enhancements" / refactor cleanup-scripts

--- cleanup.ps1
+++ cleanup.ps1
@@ -1,16 +1,15 @@
 # This script is used to clean up the project from all demo files
 # to have a clean project to start with.
 
-# print horizontal line
 Write-Host "------------------------------------------------------------------------------"
 
-# ask for confirmation to delete all demo contents, inform that this is irreversible
+# ask for confirmation to delete all demo contents
 Write-Host "This script will remove all demo related files and folders from the project."
 Write-Host "This is irreversible, are you sure you want to continue? (y/n)"
 $answer = Read-Host
 
 # if no, exit
-if ($answer -eq "n" -or $answer -eq "N") {
+if ($answer -eq "n") {
     Write-Host "Ok, exiting..."
     exit 1
 }
@@ -23,39 +22,36 @@ pnpm cleanup; pnpm postinstall
 Write-Host "Selecting files and folders to remove..."
 $files = @()
 $folders = @()
-$files += Get-ChildItem -Path . -Recurse -File -Filter "*demo*" -Exclude "*/node_modules/*" | Select-Object -ExpandProperty FullName
-$folders += Get-ChildItem -Path . -Recurse -Directory -Filter "*demo*" -Exclude "*/node_modules/*" | Select-Object -ExpandProperty FullName
+Get-ChildItem -Path . -Recurse -Filter "*demo*" -Exclude "node_modules" -File | ForEach-Object { $files += $_.FullName }
+Get-ChildItem -Path . -Recurse -Filter "*demo*" -Exclude "node_modules" -Directory | ForEach-Object { $folders += $_.FullName }
 
-# select all files within: "assets/scss/"
-$files += Get-ChildItem -Path .\assets\scss -Recurse -File | Select-Object -ExpandProperty FullName
+$markForDeletion = @(".DS_Store", "assets/scss", "e2e", "locales", "docs", "public", "server", "components", "composables", "middleware", "stores")
 
-# select all files within: "e2e/"
-$files += Get-ChildItem -Path .\e2e -Recurse -File | Select-Object -ExpandProperty FullName
+foreach ($folder in $markForDeletion) {
+    Get-ChildItem -Path $folder -Recurse -File | ForEach-Object { $files += $_.FullName }
+    Get-ChildItem -Path $folder -Recurse -Directory | ForEach-Object { $folders += $_.FullName }
+    $folders += $folder
+}
 
-# select all files within: "locales/"
-$files += Get-ChildItem -Path .\locales -Recurse -File | Select-Object -ExpandProperty FullName
-
-# select all files within: "docs/"
-$files += Get-ChildItem -Path .\docs -Recurse -File | Select-Object -ExpandProperty FullName
-
-# select all files within: "docs/"
-$files += Get-ChildItem -Path .\public -Recurse -File | Select-Object -ExpandProperty FullName
-
-# select stray files and folders
-$files += "pages\index.vue", "i18n.config.js", "TODO.md", "README.md"
-$folders += ".DS_Store", "components", "composables", "docs", "e2e", "locales", "middleware", "public", "server", "stores"
+# select stray files
+$files += "pages/index.vue", "i18n.config.js", "TODO.md", "README.md"
 
 # remove all selected files and folders
 Write-Host "Removing files and folders..."
-foreach ($file in $files) { Remove-Item -Path $file -Force -Recurse }
-foreach ($folder in $folders) { Remove-Item -Path $folder -Force -Recurse }
+foreach ($file in $files) {
+    Remove-Item -Path $file -Force -Recurse
+}
+foreach ($folder in $folders) {
+    Remove-Item -Path $folder -Force -Recurse
+}
 
 # create new: "assets/scss/app.scss" file with no content
-Write-Host "Generating clean: `"`"assets/scss/app.scss`"`"" 
-New-Item -ItemType File -Path .\assets\scss\app2.scss -Force
+Write-Host "Generating clean: `"`"assets/scss/app.scss`"`""
+mkdir "assets/scss" | Out-Null
+New-Item -ItemType File "assets/scss/app.scss" | Out-Null
 
 # create new: "pages/index.vue" file with default vue content
-Write-Host "Generating clean: `"`"pages/index.vue`"`"" 
+Write-Host "Generating clean: `"`"pages/index.vue`"`""
 @"
 <script setup lang="ts"></script>
 
@@ -64,27 +60,27 @@ Write-Host "Generating clean: `"`"pages/index.vue`"`""
 </template>
 
 <style scoped lang="scss"></style>
-"@ | Out-File -FilePath .\pages\index2.vue -Force -Encoding utf8
+"@ | Out-File -FilePath "pages/index.vue" -Encoding utf8
 
 # create new: "i18n.config.js" file with default nuxt-i18n config
-echo "Generating clean: \"i18n.config.js\"" 
+Write-Host "Generating clean: `"`"i18n.config.js`"`""
 @"
 export default {
   // https://i18n.nuxtjs.org/options-reference/
 }
-"@ | Out-File -FilePath i18n.config2.js -Encoding utf8
+"@ | Out-File -FilePath "i18n.config.js" -Encoding utf8
 
 # ask if the user wants to create clean env files
-echo "Do you want to create clean .env files? (y/n)"
+Write-Host "Do you also want to create clean .env files? (y/n)"
 $answer = Read-Host
 
 # if yes, create clean env files
-if ($answer.ToLower() -eq "y") {
-    echo "Generating clean: \".env files\""
-    Copy-Item -Path .env -Destination .env.development2
-    Copy-Item -Path .env -Destination .env.staging2
-    Copy-Item -Path .env -Destination .env.production2
+if ($answer -eq "y" -or $answer -eq "Y") {
+  Write-Host "Generating clean: \".env files\""
+  Copy-Item -Path ".env" -Destination ".env.development"
+  Copy-Item -Path ".env" -Destination ".env.staging"
+  Copy-Item -Path ".env" -Destination ".env.production"
 }
 
-echo "Done! Enjoy your clean Nuxt3-Starter-Kit! :)"
-echo "------------------------------------------------------------------------------"
+Write-Host "Done! Enjoy your clean Nuxt3-Starter-Kit! :)"
+Write-Host "------------------------------------------------------------------------------"
--- cleanup.sh
+++ cleanup.sh
@@ -1,18 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script is used to clean up the project from all demo files
 # to have a clean project to start with.
 
-# print horizontal line
 echo "------------------------------------------------------------------------------"
 
-# ask for confirmation to delete all demo contents, inform that this is irreversible
+# ask for confirmation to delete all demo contents
 echo "This script will remove all demo related files and folders from the project."
 echo "This is irreversible, are you sure you want to continue? (y/n)"
 read -r answer
 
 # if no, exit
-if [ "$answer" != "${answer#[Nn]}" ] ;then
+if [[ "$answer" =~ ^[Nn]$ ]]; then
     echo "Ok, exiting..."
     exit 1
 fi
@@ -23,61 +22,72 @@ pnpm cleanup && pnpm postinstall
 
 # select *demo* files and folders
 echo "Selecting files and folders to remove..."
-files+=( $(find . -type f -name "*demo*" -not -path "*/node_modules/*" -print) )
-folders+=( $(find . -type d -name "*demo*" -not -path "*/node_modules/*" -print) )
+declare -a files=()
+declare -a folders=()
+while IFS= read -r -d '' file; do
+    files+=("$file")
+done < <(find . -type f -name "*demo*" -not -path "*/node_modules/*" -print0)
+while IFS= read -r -d '' folder; do
+    folders+=("$folder")
+done < <(find . -type d -name "*demo*" -not -path "*/node_modules/*" -print0)
 
-# select all files within: "assets/scss/"
-files+=( $(find assets/scss -type f -print) )
+markForDeletion=( ".DS_Store" "assets/scss" "e2e" "locales" "docs" "public" "server" "components" "composables" "middleware" "stores" )
 
-# select all files within: "e2e/"
-files+=( $(find e2e -type f -print) )
+for folder in "${markForDeletion[@]}"; do
+    while IFS= read -r -d '' file; do
+        files+=("$file")
+    done < <(find "$folder" -type f -print0)
+    while IFS= read -r -d '' folder; do
+        folders+=("$folder")
+    done < <(find "$folder" -type d -print0)
+    folders+=("$folder")
+done
 
-# select all files within: "locales/"
-files+=( $(find locales -type f -print) )
-
-# select all files within: "docs/"
-files+=( $(find docs -type f -print) )
-
-# select all files within: "docs/"
-files+=( $(find public -type f -print) )
-
-# select stray files and folders
-files+=( "pages/index.vue", "i18n.config.js", "TODO.md", "README.md" )
-folders+=( ".DS_Store", "components", "composables", docs", "e2e, "locales", "middleware", "public", "server", "stores")
+# select stray files
+files+=( "pages/index.vue" "i18n.config.js" "TODO.md" "README.md" )
 
 # remove all selected files and folders
 echo "Removing files and folders..."
-for file in "${files[@]}"; do rm -rf $file; done
-for folder in "${folders[@]}"; do rm -rf $folder; done
+for file in "${files[@]}"; do
+    rm -rf "$file"
+done
+for folder in "${folders[@]}"; do
+    rm -rf "$folder"
+done
 
 # create new: "assets/scss/app.scss" file with no content
-echo "Generating clean: \"assets/scss/app.scss\"" 
-touch assets/scss/app2.scss
+echo "Generating clean: \"assets/scss/app.scss\""
+mkdir -p "assets/scss"
+touch "assets/scss/app.scss"
 
 # create new: "pages/index.vue" file with default vue content
-echo "Generating clean: \"pages/index.vue\"" 
-echo "<script setup lang=\"ts\"></script>
+echo "Generating clean: \"pages/index.vue\""
+cat > "pages/index.vue" <<EOF
+<script setup lang="ts"></script>
 
 <template>
   <h1>Enjoy your clean Nuxt3-Starter-Kit!</h1>
 </template>
 
-<style scoped lang=\"scss\"></style>" > pages/index2.vue
+<style scoped lang="scss"></style>
+EOF
 
-# chreate new: "i18n.config.js" file with default nuxt-i18n config
-echo "Generating clean: \"i18n.config.js\"" 
-echo "export default {
+# create new: "i18n.config.js" file with default nuxt-i18n config
+echo "Generating clean: \"i18n.config.js\""
+cat > "i18n.config.js" <<EOF
+export default {
   // https://i18n.nuxtjs.org/options-reference/
-}" > i18n.config2.js
+}
+EOF
 
 # ask if the user wants to create clean env files
-echo "Do you want to create clean .env files? (y/n)"
+echo "Do you also want to create clean .env files? (y/n)"
 read -r answer
 
 # if yes, create clean env files
-if [ "$answer" != "${answer#[Yy]}" ] ;then
-    echo "Generating clean: \".env files\""
-    for target in .env.development2 .env.staging2 .env.production2; do cp .env $target; done
+if [[ "$answer" =~ ^[Yy]$ ]]; then
+  echo "Generating clean: \".env files\""
+  for target in .env.development .env.staging .env.production; do cp .env $target; done
 fi
 
 echo "Done! Enjoy your clean Nuxt3-Starter-Kit! :)"
